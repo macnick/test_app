ee5e9600b3de01d866254edbc9c5dae0ae21066d
macnick <nick@macexperts.gr>
2019-12-03 13:59:20 +0200

    Sign up new users but flash messages not work

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :articles
   root 'welcome#home'
   get 'about', to: 'welcome#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  
 end
