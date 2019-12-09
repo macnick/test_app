6c30d4d8f6454f71fead57fb27aecfaae6f3428c
macnick <nick@macexperts.gr>
2019-12-09 17:06:09 +0200

    Add admin user functionality to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -72,7 +72,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can edit or delete your own articles"
         redirect_to root_path
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_094613) do
+ActiveRecord::Schema.define(version: 2019_12_09_144806) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_094613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
